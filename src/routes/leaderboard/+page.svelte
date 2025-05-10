2fd11b4f39607f66c43da00033110a05bb14998a
Liam Desic <me@liamdesic.co>
2025-05-11 08:43:35 +1000

    Update +page.svelte

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -248,10 +248,10 @@
 </div>
 
 
-</div>
+
 <div class="event-message" style="display:inline; font-size: 2rem; padding: 1.5rem; background: none !important;">
 <h3 style="background: none !important;"> Mothers Day Gift for 1st Place at the start of every hour </h3>
-
+</div>
 
 
 
